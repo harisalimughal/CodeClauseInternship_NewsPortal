7cf1fe2a48e634c28a79bc5370227a9166b9b3bd
harisalimughal <harisalimughal077@gmail.com>
2026-05-21 15:10:41 +0500

    Remove unused Link import from FetchData component

--- src/components/FetchData.js
+++ src/components/FetchData.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
 
 const FetchData = ({ cat }) => {
